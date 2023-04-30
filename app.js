ad6e1e35363e57ef7d3ef6c36b90881dddec2ca8
Aleksey Abamzarov <alekseu2308@mail.ru>
2023-04-30 09:37:36 +0300

    feat: pockets and db

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/films', require('./routes/films'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер');
+  console.log(BASE_PATH);
+});
